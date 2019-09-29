07344262f865bfbd30d8c837ca7396014b2f364a
octoxalis <octoxalis@gmail.com>
2019-09-29 18:38:20 +0200

    refactoring templates (includes) dir

--- @workspace/ins_note.js
+++
@@ -1,8 +0,0 @@
-
-  /*
-   {% inlineNote %}__content__{% endinlineNote %}
-   */
-  inlineNote__s: ( content_s ) =>
-  `<ins class="ca_note_ins" onclick="this.getElementsByClassName('ca_note_content')[0].classList.toggle('ca_note_open')"><sup></sup><span id="note_1" class="ca_note_content">${content_s}</span></ins>`,
-  
-  config.addPairedShortcode("inlineNote",        ( content_s, note_n ) => CODES_o.inlineNote__s( content_s, note_n ) )
