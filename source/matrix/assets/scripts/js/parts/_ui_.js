558de99629aa15fbcea4d81f052d943531a5c37d
octoxalis <octoxalis@gmail.com>
2019-12-19 10:01:50 +0100

    refactore menu

--- source/matrix/assets/scripts/js/parts/_ui_.js
+++ source/matrix/assets/scripts/js/parts/_ui_.js
@@ -40,22 +40,24 @@ const linkNear__o = link_s =>
   const extension_n = '.html'.length
   const location_s = window.location.pathname.slice( 1, -extension_n )  //: trim '/' at start
   const list_a = document.querySelectorAll( `[data--="menu_list"] > li` )
+  if ( !list_a ) return     //: undefined
   const list_n = list_a.length
   const current_e = list_e.querySelector( `[data-link="${location_s}"]` )
   const rank_n = +current_e.getAttribute( 'data-rank' )
   const near_n = ( link_s === 'link_previous' ) ? rank_n - 1 : rank_n + 1
-  if ( near_n < 1 || near_n > list_n ) return undefined
+  if ( near_n < 1 || near_n > list_n ) return     //: undefined
   const near_e = list_e.querySelector( `[data-rank="${near_n}"]` )
+  if ( !near_e ) return     //: undefined
   const a_e = near_e.querySelector( `span > a` )
+  if ( !a_e ) return     //: undefined
   const span_e = near_e.querySelector( `span[data--="note_content"]` )
-  const near_o =
-  {
+  if ( !a_e ) return     //: undefined
+  return {
     link_s:     near_e.getAttribute( 'data-link' ),
     title_s:    a_e.innerHTML,
     //?? subtitle_s: '',
     abstract_s: span_e.innerHTML,
   }
-  return near_o
 }
 
 const linkNear__v = ( event_s, link_e ) =>
@@ -86,7 +88,7 @@ const linkNear__v = ( event_s, link_e ) =>
     .classList.toggle( 'retract' )
 }
 
-const linkPageURL__s = link_s =>
+const linkURL__s = link_s =>
 {
   const link_o = linkNear__o( link_s )
   return link_o ? `{{U_o.url_s}}${link_o.link_s}.html` : undefined
@@ -160,7 +162,7 @@ void function ()
       let link_s = link_e.getAttribute( 'data--' )
       if ( link_s === 'link_menu' ) return void menu__v()
       //: previous or next tip
-      const http_s = linkPageURL__s( link_s )
+      const http_s = linkURL__s( link_s )
       if ( http_s ) window.location = http_s
     } )
 
