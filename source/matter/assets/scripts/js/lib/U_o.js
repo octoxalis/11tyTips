1766ae591ec768ba22ef1db0f6f8686bf62fc302
octoxalis <octoxalis@gmail.com>
2019-12-27 08:15:32 +0100

    modify <a> css

--- source/matter/assets/scripts/js/lib/U_o.js
+++ source/matter/assets/scripts/js/lib/U_o.js
@@ -19,14 +19,15 @@ const U_o =
   SERVICE_PATH_s: 'assets/scripts/js/service_worker.min.js',
 
   HOME_s:    `[Home page]: ${A_o.URL_s}`,
-  NODE_s :   `[Node.js]: https://nodejs.org`,
   COMMENT_s: `[utteranc.es]: https://github.com/utterance/utterances`,
+  NETLIFY_s: `https://www.netlify.com`,
+  NODE_s :   `[Node.js]: https://nodejs.org`,
 
   FRAME_s:     '11ty Frame',
   FRAME_URL_s: `https://11tyframe.netlify.com/`,
 
   ELEVENTY_s:     `[Eleventy]: https://11ty.io`,
-  ELEVENTY_DEV_s:     `[Eleventy]: https://11ty.dev`,
+  ELEVENTY_DEV_s: `[Eleventy]: https://11ty.dev`,
   ELEVENTY_JFM_s: `/docs/data-frontmatter/#javascript-front-matter`,
   ELEVENTY_UDF_s: `/docs/data-frontmatter/#user-defined-front-matter-customizations`,
   ELEVENTY_SORT_s: `/docs/collections/#sorting`,
